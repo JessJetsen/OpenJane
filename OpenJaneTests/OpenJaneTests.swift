93c35d115423db1620538623892e867b8c6741a3
thewizardofthenorth <thewizard@thetinkeringwizard.com>
2026-04-22 10:14:46 -0600

    Initial Commit

---
+++ OpenJaneTests/OpenJaneTests.swift
@@ -0,0 +1,19 @@
+//
+//  OpenJaneTests.swift
+//  OpenJaneTests
+//
+//  Created by Jens Jensen on 2026-04-22.
+//
+
+import Testing
+@testable import OpenJane
+
+struct OpenJaneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
